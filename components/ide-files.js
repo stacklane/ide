38fba900695e5eb48c9ca65d9893b26dee3e0825
Ryan Dewell <ryan@dewell.org>
2020-04-02 10:36:44 -0600

    Milestone

--- components/ide-files.js
+++ components/ide-files.js
@@ -2,6 +2,23 @@
 const TOUCH_DEVICE = 'ontouchstart' in document.documentElement;
 const FILE_NODE_CLICK_EVENT = TOUCH_DEVICE ? 'touchstart' : 'dblclick';
 
+const KEY_CODE = Object.freeze({
+    RETURN: 13,
+    SPACE: 32,
+    PAGEUP: 33,
+    PAGEDOWN: 34,
+    END: 35,
+    HOME: 36,
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40
+});
+
+const isPrintableCharacter = (str)=>{
+    return str.length === 1 && str.match(/\S/);
+};
+
 class FileLink extends IDEComponent{
     constructor(fileId, fileName) {
         super();
@@ -77,33 +94,51 @@ class FileDirToggle extends HTMLElement{
     }
 }
 
+const MOUSE_OVER = function(e){
+    e.currentTarget.classList.add('hover');
+    e.stopPropagation();
+};
+const MOUSE_OUT = function(e){
+    e.currentTarget.classList.remove('hover');
+    e.stopPropagation();
+};
+
+
 class FileDir extends IDEComponent{
     constructor(name, path, expandable) {
         super();
         this._name = name;
-        this._expandable = expandable; // aka user-expandable
+        this._expandable = expandable; // AKA "user expandable"
         this._path = path;
 
+        this.setAttribute('aria-expanded', 'false');
         this.setAttribute('role', 'treeitem');
 
         this.expanded = !this._expandable; // expanded now, if not user-expandable
 
         if (this._name){
-            let labelElement = document.createElement('span');
-            if (this._expandable) labelElement.appendChild(new FileDirToggle(this));
-            let ll = document.createElement('span');
-            ll.innerText = this._name;
-            labelElement.appendChild(ll);
-            this.appendChild(labelElement);
+            const labelControl = document.createElement('span');
+            if (this.isExpandable()) {
+                labelControl.appendChild(new FileDirToggle(this));
+                this.addEventListener('focus', ()=>labelControl.classList.add('focus'));
+                this.addEventListener('blur', ()=>labelControl.classList.remove('focus'));
+                labelControl.addEventListener('mouseover', MOUSE_OVER);
+                labelControl.addEventListener('mouseout', MOUSE_OUT);
+            }
+            const labelText = document.createElement('span');
+            labelText.innerText = this._name;
+            labelControl.appendChild(labelText);
+            this.appendChild(labelControl);
         }
 
-        let d = document.createElement('div');
-        d.setAttribute('role', 'group');
-        this._group = d;
-        this.appendChild(d);
+        const groupDiv = document.createElement('div');
+        groupDiv.setAttribute('role', 'group');
+        this._group = groupDiv;
+        this.appendChild(groupDiv);
 
-        if (this._expandable) {
+        if (this.isExpandable()) {
             const that = this;
+
             const toggleEvent = function (e) {
                 that.toggle();
                 e.stopPropagation();
@@ -114,24 +149,20 @@ class FileDir extends IDEComponent{
         }
     }
 
+    get childDirs(){
+        return Array.from(this._group.children).filter((c)=>c instanceof FileDir);
+    }
+
     /**
      * Add nested FileDir, or return existing.
      */
     addDir(name){
         const group = this._group;
 
-        // 1) Check if FileDir with same name already exists:
-        for (let i = 0; i < group.children.length; i++){
-            let c = group.children.item(i);
-            if (c instanceof FileDir && c.name === name) return c;
-        }
-
-        // 2) Find where it should go relative to existing children:
-        //const beforeNode = this._findBeforeNode(newDir);
-        //group.insertBefore(newDir, beforeNode);
+        const existing = this.childDirs.find((dir)=>dir.name === name);
+        if (existing) return existing;
 
         const newDir = new FileDir(name, this._path + name + '/', true);
-
         group.appendChild(newDir);
 
         this._sort();
@@ -146,30 +177,6 @@ class FileDir extends IDEComponent{
             .sort((a, b) => a.compareTo(b))
             .forEach(el => group.appendChild(el));
     }
-
-    /**
-     * For #insertBefore -- node where the new FileDir or FileItem should be placed before, or null.
-
-    _findBeforeNode(newDirOrItem){
-        const group = this._group;
-
-        let insertBefore = null; // OK to be null, same as insertBefore
-
-        for (let i = 0; i < group.children.length; i++){
-            const next = group.children.item(i);
-            const cmp = newDirOrItem.compareTo(next);
-
-            console.log(newDirOrItem + ' compareTo ' + next + ' == ' + cmp);
-
-            if (cmp < 0){
-                break;
-            } else {
-                insertBefore = next;
-            }
-        }
-
-        return before;
-    }*/
 
     toString(){
         return 'FileDir[' + this._path + ']';
@@ -187,12 +194,16 @@ class FileDir extends IDEComponent{
 
     addItem(fileId, fileName){
         const fileItem = new FileItem(fileId, fileName, this._path + fileName);
-
-        //const before = this._findBeforeNode(fileItem);
-        //this._group.insertBefore(fileItem, before);
-
         this._group.appendChild(fileItem);
         this._sort();
+    }
+
+    isExpanded(){
+        return this.expanded;
+    }
+
+    isExpandable(){
+        return this._expandable;
     }
 
     get path(){
@@ -207,12 +218,25 @@ class FileDir extends IDEComponent{
         this.expanded = !this.expanded;
     }
 
+    collapse(){
+        this.expanded = false;
+    }
+
+    expand(){
+        this.expanded = true;
+    }
+
     get expanded(){
         return this.getAttribute('aria-expanded')==='true';
     }
 
     set expanded(val){
+        if (val === this.expanded) return;
+
         this.setAttribute('aria-expanded', (val === true || val === 'true') ? 'true' : 'false');
+
+        // When collapsing this FileDir, then also collapse children (which will recursively keep collapsing):
+        if (!val) this.childDirs.forEach((child)=>child.collapse());
     }
 }
 
@@ -238,11 +262,24 @@ class FileItem extends IDEComponent{
             this.addEventListener('keydown', function (e) {
                 if (e.code === 'Enter') handleOpen(e);
             });
+
+            this.addEventListener('focus', ()=>that.classList.add('focus'));
+            this.addEventListener('blur', ()=>that.classList.remove('focus'));
+            this.addEventListener('mouseover',  MOUSE_OVER);
+            this.addEventListener('mouseout', MOUSE_OUT);
         }
     }
 
     toString(){
         return 'FileItem[' + this._path + ']';
+    }
+
+    isExpanded(){
+        return false; // because leaf node
+    }
+
+    isExpandable(){
+        return false; // because leaf node
     }
 
     get path(){
@@ -401,7 +438,7 @@ TreeLinks.prototype.setFocusToLastItem = function () {
 TreeLinks.prototype.expandTreeitem = function (currentItem) {
 
     if (currentItem.isExpandable) {
-        currentItem.domNode.setAttribute('aria-expanded', true);
+        currentItem.domNode.expand();
         this.updateVisibleTreeitems();
     }
 
@@ -430,7 +467,7 @@ TreeLinks.prototype.collapseTreeitem = function (currentItem) {
     }
 
     if (groupTreeitem) {
-        groupTreeitem.domNode.setAttribute('aria-expanded', false);
+        groupTreeitem.domNode.collapse();
         this.updateVisibleTreeitems();
         this.setFocusToItem(groupTreeitem);
     }
@@ -449,7 +486,7 @@ TreeLinks.prototype.updateVisibleTreeitems = function () {
         ti.isVisible = true;
 
         while (parent && (parent !== this.domNode)) {
-            if (parent.getAttribute('aria-expanded') === 'false') {
+            if (parent instanceof FileDir && !parent.isExpanded()) {
                 ti.isVisible = false;
             }
             parent = parent.parentNode;
@@ -520,83 +557,41 @@ TreeLinks.prototype.getIndexFirstChars = function (startIndex, char) {
 var TreeitemLink = function (node, treeObj, group) {
 
     node.tabIndex = -1;
+
     this.tree = treeObj;
     this.groupTreeitem = group;
     this.domNode = node;
+
+    if (this.domNode.tagName.toLowerCase() !== 'ide-file-dir' &&
+        this.domNode.tagName.toLowerCase() !== 'ide-file-item'){
+        throw "! ide-file-dir && ! ide-file-item was " + this.domNode.tagName;
+    }
+
     this.label = node.textContent.trim();
-    this.stopDefaultClick = false;
 
     if (node.getAttribute('aria-label')) {
         this.label = node.getAttribute('aria-label').trim();
     }
 
-    this.isExpandable = false;
+    this.isExpandable = this.domNode.isExpandable();
     this.isVisible = false;
     this.inGroup = false;
 
     if (group) {
         this.inGroup = true;
     }
-
-    var elem = node.firstElementChild;
-
-    while (elem) {
-
-        if (elem.getAttribute('role') === 'group') {
-            this.isExpandable = true;
-            break;
-        }
-
-        elem = elem.nextElementSibling;
-    }
-
-    this.keyCode = Object.freeze({
-        RETURN: 13,
-        SPACE: 32,
-        PAGEUP: 33,
-        PAGEDOWN: 34,
-        END: 35,
-        HOME: 36,
-        LEFT: 37,
-        UP: 38,
-        RIGHT: 39,
-        DOWN: 40
-    });
 };
 
 TreeitemLink.prototype.init = function () {
     this.domNode.tabIndex = -1;
-
-    if (this.domNode.getAttribute('role') !== 'treeitem') throw '!treeitem';
-
     this.domNode.addEventListener('keydown', this.handleKeydown.bind(this));
-    //this.domNode.addEventListener('click', this.handleClick.bind(this));
-    this.domNode.addEventListener('focus', this.handleFocus.bind(this));
-    this.domNode.addEventListener('blur', this.handleBlur.bind(this));
-
-    // use :hover and CSS classes as needed:
-    //if (this.isExpandable) {
-    //    this.domNode.firstElementChild.addEventListener('mouseover', this.handleMouseOver.bind(this));
-    //    this.domNode.firstElementChild.addEventListener('mouseout', this.handleMouseOut.bind(this));
-    //} else {
-    //    this.domNode.addEventListener('mouseover', this.handleMouseOver.bind(this));
-    //   this.domNode.addEventListener('mouseout', this.handleMouseOut.bind(this));
-    // }
 };
 
-/* any ARIA reason not use :hover?
-TreeitemLink.prototype.handleMouseOver = function (event) {
-    event.currentTarget.classList.add('hover');
-};
-
-TreeitemLink.prototype.handleMouseOut = function (event) {
-    event.currentTarget.classList.remove('hover');
-};*/
 
 TreeitemLink.prototype.isExpanded = function () {
 
     if (this.isExpandable) {
-        return this.domNode.getAttribute('aria-expanded') === 'true';
+        return this.domNode.isExpanded();
     }
 
     return false;
@@ -606,14 +601,15 @@ TreeitemLink.prototype.isExpanded = function () {
 /* EVENT HANDLERS */
 
 TreeitemLink.prototype.handleKeydown = function (event) {
-    var tgt = event.currentTarget,
+    let tgt = event.currentTarget,
         flag = false,
         char = event.key,
         clickEvent;
 
-    function isPrintableCharacter (str) {
-        return str.length === 1 && str.match(/\S/);
-    }
+    // Looks strange to retain hover during keyboard navigation:
+    this.tree.domNode.querySelectorAll('.hover').forEach((e)=>{
+        e.classList.remove('hover');
+    });
 
     function printableCharacter (item) {
         if (char === '*') {
@@ -628,17 +624,12 @@ TreeitemLink.prototype.handleKeydown = function (event) {
         }
     }
 
-    this.stopDefaultClick = false;
-
-    if (event.altKey || event.ctrlKey || event.metaKey) {
-        return;
-    }
+    if (event.altKey || event.ctrlKey || event.metaKey) return;
 
     if (event.shift) {
-        if (event.keyCode === this.keyCode.SPACE ||
-            event.keyCode === this.keyCode.RETURN) {
+        if (event.keyCode === KEY_CODE.SPACE ||
+            event.keyCode === KEY_CODE.RETURN) {
             event.stopPropagation();
-            this.stopDefaultClick = true;
         }
         else {
             if (isPrintableCharacter(char)) {
@@ -647,8 +638,8 @@ TreeitemLink.prototype.handleKeydown = function (event) {
         }
     } else {
         switch (event.keyCode) {
-            case this.keyCode.SPACE:
-            case this.keyCode.RETURN:
+            case KEY_CODE.SPACE:
+            case KEY_CODE.RETURN:
                 if (this.isExpandable) {
                     if (this.isExpanded()) {
                         this.tree.collapseTreeitem(this);
@@ -660,21 +651,20 @@ TreeitemLink.prototype.handleKeydown = function (event) {
                 }
                 else {
                     event.stopPropagation();
-                    this.stopDefaultClick = true;
                 }
                 break;
 
-            case this.keyCode.UP:
+            case KEY_CODE.UP:
                 this.tree.setFocusToPreviousItem(this);
                 flag = true;
                 break;
 
-            case this.keyCode.DOWN:
+            case KEY_CODE.DOWN:
                 this.tree.setFocusToNextItem(this);
                 flag = true;
                 break;
 
-            case this.keyCode.RIGHT:
+            case KEY_CODE.RIGHT:
                 if (this.isExpandable) {
                     if (this.isExpanded()) {
                         this.tree.setFocusToNextItem(this);
@@ -686,7 +676,7 @@ TreeitemLink.prototype.handleKeydown = function (event) {
                 flag = true;
                 break;
 
-            case this.keyCode.LEFT:
+            case KEY_CODE.LEFT:
                 if (this.isExpandable && this.isExpanded()) {
                     this.tree.collapseTreeitem(this);
                     flag = true;
@@ -699,12 +689,12 @@ TreeitemLink.prototype.handleKeydown = function (event) {
                 }
                 break;
 
-            case this.keyCode.HOME:
+            case KEY_CODE.HOME:
                 this.tree.setFocusToFirstItem();
                 flag = true;
                 break;
 
-            case this.keyCode.END:
+            case KEY_CODE.END:
                 this.tree.setFocusToLastItem();
                 flag = true;
                 break;
@@ -722,40 +712,4 @@ TreeitemLink.prototype.handleKeydown = function (event) {
         event.preventDefault();
     }
 };
-
-/*
-TreeitemLink.prototype.handleClick = function (event) {
-
-    // only process click events that directly happened on this treeitem
-    if (event.target !== this.domNode && event.target !== this.domNode.firstElementChild) {
-        return;
-    }
-
-    if (this.isExpandable) {
-        if (this.isExpanded()) {
-            this.tree.collapseTreeitem(this);
-        }
-        else {
-            this.tree.expandTreeitem(this);
-        }
-        event.stopPropagation();
-    }
-};*/
-
-TreeitemLink.prototype.handleFocus = function (event) {
-    var node = this.domNode;
-    if (this.isExpandable) {
-        node = node.firstElementChild;
-    }
-    node.classList.add('focus');
-};
-
-TreeitemLink.prototype.handleBlur = function (event) {
-    var node = this.domNode;
-    if (this.isExpandable) {
-        node = node.firstElementChild;
-    }
-    node.classList.remove('focus');
-};
-
 
